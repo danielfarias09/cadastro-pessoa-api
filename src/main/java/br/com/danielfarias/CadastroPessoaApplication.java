00d5cd4c10edf13943204ffccecbfd61e4fe919b
Daniel Farias <daniel.graciano@quantaconsultoria.com>
2018-09-28 17:21:14 -0300

    Corrigindo restante dos métodos do controller

--- src/main/java/br/com/danielfarias/CadastroPessoaApplication.java
+++ src/main/java/br/com/danielfarias/CadastroPessoaApplication.java
@@ -2,9 +2,10 @@ package br.com.danielfarias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CadastroPessoaApplication {
+public class CadastroPessoaApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroPessoaApplication.class, args);
