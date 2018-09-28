9d3e2530840e185d84bf7efa55287271e614093c
Daniel Farias <daniel.graciano@quantaconsultoria.com>
2018-09-28 15:38:05 -0300

    Corrigindo restante dos métodos do controller

--- src/main/java/br/com/danielfarias/controller/PessoaController.java
+++ src/main/java/br/com/danielfarias/controller/PessoaController.java
@@ -7,12 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.com.danielfarias.model.Pessoa;
 import br.com.danielfarias.model.Telefone;
 import br.com.danielfarias.repository.PessoaRepository;
@@ -24,22 +24,22 @@ public class PessoaController {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
-	@RequestMapping(value="/",method = RequestMethod.GET)
-	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(method = RequestMethod.GET)
+	//@CrossOrigin(origins = "http://localhost:4200")
 	public List<Pessoa> list(@Param("nome") String nome, @Param("cpf") String cpf) {
 		return getPessoa(nome,cpf);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, consumes="application/json")
-	@CrossOrigin(origins = "http://localhost:4200")
+	//@CrossOrigin(origins = "http://localhost:4200")
 	public ResponseEntity<HttpStatus> create(@RequestBody Pessoa pessoa) {
 		atribuiTelefonePessoa(pessoa);
 		pessoaRepository.save(pessoa);
 		return ResponseEntity.ok().body(HttpStatus.OK);
 	}
 	
-	@RequestMapping( method = RequestMethod.PUT, consumes="application/json")
-	@CrossOrigin(origins = "http://localhost:4200")
+
+	//@CrossOrigin(origins = "http://localhost:4200")
 	public ResponseEntity<HttpStatus> update(@RequestBody Pessoa pessoa) {
 		atribuiTelefonePessoa(pessoa);
 		pessoaRepository.save(pessoa);
@@ -47,13 +47,13 @@ public class PessoaController {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	@CrossOrigin(origins = "http://localhost:4200")
+	//@CrossOrigin(origins = "http://localhost:4200")
 	public Optional<Pessoa> get(@PathVariable("id") Long id) {
 		return pessoaRepository.findById(id);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	@CrossOrigin(origins = "http://localhost:4200")
+	//@CrossOrigin(origins = "http://localhost:4200")
 	public ResponseEntity<HttpStatus> delete(@PathVariable("id") Long id) {
 		pessoaRepository.deleteById(id);
 		return ResponseEntity.ok().body(HttpStatus.OK);
