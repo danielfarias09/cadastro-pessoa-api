c760cdf8e7a61deb7c7c0c911b1f7f92a7f652b7
Daniel Farias <daniel.graciano@quantaconsultoria.com>
2018-10-02 11:17:48 -0300

    Corrigindo restante dos métodos do controller

--- src/main/java/br/com/danielfarias/controller/PessoaController.java
+++ src/main/java/br/com/danielfarias/controller/PessoaController.java
@@ -25,21 +25,17 @@ public class PessoaController {
 	private PessoaRepository pessoaRepository;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	//@CrossOrigin(origins = "http://localhost:4200")
 	public List<Pessoa> list(@Param("nome") String nome, @Param("cpf") String cpf) {
 		return getPessoa(nome,cpf);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, consumes="application/json")
-	//@CrossOrigin(origins = "http://localhost:4200")
 	public ResponseEntity<HttpStatus> create(@RequestBody Pessoa pessoa) {
 		atribuiTelefonePessoa(pessoa);
 		pessoaRepository.save(pessoa);
 		return ResponseEntity.ok().body(HttpStatus.OK);
 	}
 	
-
-	//@CrossOrigin(origins = "http://localhost:4200")
 	public ResponseEntity<HttpStatus> update(@RequestBody Pessoa pessoa) {
 		atribuiTelefonePessoa(pessoa);
 		pessoaRepository.save(pessoa);
@@ -47,13 +43,11 @@ public class PessoaController {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	//@CrossOrigin(origins = "http://localhost:4200")
 	public Optional<Pessoa> get(@PathVariable("id") Long id) {
 		return pessoaRepository.findById(id);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	//@CrossOrigin(origins = "http://localhost:4200")
 	public ResponseEntity<HttpStatus> delete(@PathVariable("id") Long id) {
 		pessoaRepository.deleteById(id);
 		return ResponseEntity.ok().body(HttpStatus.OK);
