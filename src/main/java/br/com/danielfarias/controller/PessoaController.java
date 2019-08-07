b8f3c98df95e8abb33f436ac527ed5ceb8e4d5d1
unknown <daniel.farias@MATRIZ-186125.hapvida.com.br>
2019-08-07 17:28:53 -0300

    Primeiro commit

--- src/main/java/br/com/danielfarias/controller/PessoaController.java
+++ src/main/java/br/com/danielfarias/controller/PessoaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import br.com.danielfarias.to.PessoaTO;
 
 @RestController
 @RequestMapping("/pessoas")
+@CrossOrigin(origins="*")
 public class PessoaController {
 	
 	@Autowired
